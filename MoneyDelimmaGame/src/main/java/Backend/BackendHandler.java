efe1357609578cfce1fd9fb592cb7e19ea80e79a
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-01 13:45:04 -0400

    updated and added servlet/project/stuff

---
+++ MoneyDelimmaGame/src/main/java/Backend/BackendHandler.java
@@ -0,0 +1,27 @@
+package Backend;
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse; 
+
+
+@WebServlet(description = "Backend Handler for Ultimatem/Dilemma Game", urlPatterns = { "/" })
+public class BackendHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public BackendHandler() {
+	    super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Welcome to the Dilemma Game");
+	}
+
+}
+
+
